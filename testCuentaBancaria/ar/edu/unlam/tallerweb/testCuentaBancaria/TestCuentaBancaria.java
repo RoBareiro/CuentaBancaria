8e8a428049d0b9a5ed75b8baf322537acd02c293
Rocho <rncastaniervivas@hotmail.com.ar>
2016-04-27 15:02:42 -0300

    agrego tests

--- testCuentaBancaria/ar/edu/unlam/tallerweb/testCuentaBancaria/TestCuentaBancaria.java
+++ testCuentaBancaria/ar/edu/unlam/tallerweb/testCuentaBancaria/TestCuentaBancaria.java
@@ -4,11 +4,33 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import ar.edu.unlam.tallerweb.cuentaBancaria.CuentaBancaria;
+
 public class TestCuentaBancaria {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void probarQueMuestreElSaldoInicial() {
+		CuentaBancaria miCuenta= new CuentaBancaria(1234);
+		Double valorEsperado= 0.00;
+		assertEquals(valorEsperado, miCuenta.getSaldoActual());
+		
+	}
+	
+	@Test
+	public void depositarPlataYConsultarSaldo(){
+		CuentaBancaria miCuenta = new CuentaBancaria(1234);
+		Double deposito = miCuenta.depositar(100.00);
+		Double valorEsperado = miCuenta.getSaldoActual();
+		assertEquals(valorEsperado, deposito);
+	}
+	
+	@Test
+	public void extraerPlataDeCuentaYConsultarSaldo(){
+		CuentaBancaria miCuenta = new CuentaBancaria(1234);
+		miCuenta.depositar(100.00);
+		Double extraccion = miCuenta.extraer(20.00);
+		Double valorEsperado = miCuenta.getSaldoActual();
+		assertEquals(valorEsperado, extraccion);
 	}
 
 }
